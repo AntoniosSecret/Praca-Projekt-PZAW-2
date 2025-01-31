5fe42aafeb17dfe08ea449f0e295b5c33ba4f65c
Antoni <antos.misztal@gmail.com>
2025-01-31 20:07:52 +0100

    Podstawy Homepage i galerii.

--- mysite/stylr/views.py
+++ mysite/stylr/views.py
@@ -3,16 +3,17 @@ import random
 
 
 rand_subheads = [
-    'Make your own style!',
-    'Start Stylizing!',
+    'Make your style!',
+    "Get Stylin'!",
     'Take some inspiration!',
     'Create your outfits!',
 ]
 
 def home(request):
     retcode = 200
+    rand_subhead = rand_subheads[random.randint(0, len(rand_subheads) - 1)]
     context = {
         'title': 'Home',
-        'rand_subhead': rand_subheads[random.randint(0, len(rand_subheads) - 1)],
+        'rand_subhead': rand_subhead,
     }
     return render(request, 'stylr/home.html', context, status=retcode)
