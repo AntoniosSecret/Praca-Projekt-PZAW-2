f7a916ee18add71212c2fc6e37a5df83e9372bd7
Antoni <antos.misztal@gmail.com>
2025-02-10 13:57:11 +0100

    Dodano models i formularz do postow.

--- mysite/stylr/admin.py
+++ mysite/stylr/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from .models import Post
+
+admin.site.unregister(Group)
+
+admin.site.register(Post)
 
 # Register your models here.
--- mysite/stylr/models.py
+++ mysite/stylr/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    image = models.ImageField(upload_to="stylr/images")
+    desc = models.CharField(max_length=200)
+    likes = models.PositiveIntegerField()
+    datetimePosted = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return (
+            f"{self.image}"
+            f"({self.datetimePosted:%d-%m-%Y %H:%M}):"
+            f"{self.desc}"
+            f"{self.likes}"
+        )
